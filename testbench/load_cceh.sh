e4e348fe0119f344e150f4f6771a034c7ec046de
Xingsheng Zhao <xingshengzhao@gmail.com>
2022-02-15 10:52:55 -0600

    add cmake settings

--- testbench/load_cceh.sh
+++ testbench/load_cceh.sh
@@ -1,7 +1,8 @@
-NUM=80000000
+NUM=40000000
 
-for t in 40
+for t in 1 20 40
 do
-# numactl -N 0 sudo ../release/bench_cceh_spoton --thread=$t --num=$NUM --benchmarks=load| tee load_cceh_spoton_$t.log
-numactl -N 0 sudo ../release/bench_cceh        --thread=$t --num=$NUM --benchmarks=load| tee load_cceh_$t.log
+numactl -N 0 sudo ../release/bench_cceh                 --thread=$t --num=$NUM --benchmarks=load| tee load_cceh_$t.log
+numactl -N 0 sudo ../release/bench_cceh_spoton_buf      --thread=$t --num=$NUM --benchmarks=load| tee load_cceh_spoton_buf_$t.log
+numactl -N 0 sudo ../release/bench_cceh_spoton_buf_otp  --thread=$t --num=$NUM --benchmarks=load| tee load_cceh_spoton_buf_otp_$t.log
 done
--- testbench/load_skiplist.sh
+++ testbench/load_skiplist.sh
@@ -1,10 +1,9 @@
+NUM=40000000
 
-
-NUM=80000000
-
-for t in 40
+for t in 1 20 40
 do
-numactl -N 0 sudo ../release/bench_skiplist_spoton --thread=$t --num=$NUM --benchmarks=load | tee load_skiplist_spoton_$t.log
-numactl -N 0 sudo ../release/bench_skiplist        --thread=$t --num=$NUM --benchmarks=load | tee load_skiplist_$t.log
+numactl -N 0 sudo ../release/bench_skiplist                     --thread=$t --num=$NUM --benchmarks=load | tee load_skiplist_$t.log
+numactl -N 0 sudo ../release/bench_skiplist_spoton_buf_otp      --thread=$t --num=$NUM --benchmarks=load | tee load_skiplist_spoton_buf_otp_$t.log
+numactl -N 0 sudo ../release/bench_skiplist_spoton_buf_otp_di   --thread=$t --num=$NUM --benchmarks=load | tee load_skiplist_spoton_buf_otp_di_$t.log
 done
 
